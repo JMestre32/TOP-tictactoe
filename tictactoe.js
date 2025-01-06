6aecbb1aa26f10936bfc144887467f90bf049c5d
JMestre32 <114640505+JMestre32@users.noreply.github.com>
2025-01-06 08:55:39 -0800

    Make game logic

--- tictactoe.js
+++ tictactoe.js
@@ -4,14 +4,16 @@ console.log("bwuh");
 
 function createGame(){
     const game = [
-        ["","",""],
-        ["","",""],
-        ["","",""]
+        ["X","O","O"],
+        ["O","X","O"],
+        ["O","X","X"]
     ]
     return {
         board: game
     }
 }
+
+const game = createGame()
 
 // Players are also going to be stored in objects
 
@@ -34,7 +36,39 @@ function createPlayer(name, player){
     }
 }
 
-// const player1 = createPlayer('jacob', 1)
-// const player2 = createPlayer('alex', 2)
+const player1 = createPlayer('jacob', 1)
+const player2 = createPlayer('alex', 2)
 // const player3 = createPlayer('bob', 3)
 // Create an object to control the flow of the game itself
+
+function gameFlow(game, player1, player2){
+
+    if(
+        (game.board[0][0] === 'X' && game.board[0][1] === 'X' && game.board[0][2] === 'X') ||
+        (game.board[1][0] === 'X' && game.board[1][1] === 'X' && game.board[1][2] === 'X') ||
+        (game.board[2][0] === 'X' && game.board[2][1] === 'X' && game.board[2][2] === 'X') ||
+        (game.board[0][0] === 'X' && game.board[1][0] === 'X' && game.board[2][0] === 'X') ||
+        (game.board[0][1] === 'X' && game.board[1][1] === 'X' && game.board[2][1] === 'X') ||
+        (game.board[0][2] === 'X' && game.board[1][2] === 'X' && game.board[2][2] === 'X') ||
+        (game.board[0][0] === 'X' && game.board[1][1] === 'X' && game.board[2][2] === 'X')
+    ){
+        console.log(player1.name + ' Wins!')
+    }
+    else if(
+        (game.board[0][0] === 'O' && game.board[0][1] === 'O' && game.board[0][2] === 'O') ||
+        (game.board[1][0] === 'O' && game.board[1][1] === 'O' && game.board[1][2] === 'O') ||
+        (game.board[2][0] === 'O' && game.board[2][1] === 'O' && game.board[2][2] === 'O') ||
+        (game.board[0][0] === 'O' && game.board[1][0] === 'O' && game.board[2][0] === 'O') ||
+        (game.board[0][1] === 'O' && game.board[1][1] === 'O' && game.board[2][1] === 'O') ||
+        (game.board[0][2] === 'O' && game.board[1][2] === 'O' && game.board[2][2] === 'O') ||
+        (game.board[0][0] === 'O' && game.board[1][1] === 'O' && game.board[2][2] === 'O')
+    ){
+        console.log(player2.name + " Wins!")
+    }
+    else if(game.board.some(row => row.includes(""))){
+        console.log('play on')
+    }
+    else{
+        console.log('tie game')
+    }
+}
