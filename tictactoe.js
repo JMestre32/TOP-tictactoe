e790165d82baf0cb211f4dbf7762a1cecf8f4881
Jacob M <114640505+JMestre32@users.noreply.github.com>
2025-04-16 11:34:18 -0700

    Add logic for ties

--- tictactoe.js
+++ tictactoe.js
@@ -4,19 +4,24 @@ const gameBoard = (function (){
     const cols = 3;
     const board = [];
     
-
-    for (let i = 0; i < rows; i++){
-        board[i] = [];
-        for(let j = 0; j < cols; j++){
-            board[i].push("0")
+    //Initialize the board with 0's
+    const resetBoard = () =>
+            {
+            for (let i = 0; i < rows; i++){
+                board[i] = [];
+                for(let j = 0; j < cols; j++){
+                    board[i].push("0")
+                }
+            }
         }
-    }
 
     const getBoard = () => board;
 
     const placeMark = (row, col, player) => {
-        if(board[row][col] === "0" && (0 < row < 2) && (0 < col < 2)){
+        if (board[row][col] === "0" && row >= 0 && row <= 2 && col >= 0 && col <= 2)
+            {
             board[row][col] = player.playerMark;
+            //flag to check if a valid move was made
             return true
         }
         else{
@@ -24,17 +29,53 @@ const gameBoard = (function (){
             return false
         }
     }
+
+    const checkWin = () => {
+        if(
+            board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X" ||
+            board[1][0] == "X" && board[1][1] == "X" && board[1][2] == "X" ||
+            board[2][0] == "X" && board[2][1] == "X" && board[2][2] == "X" ||
+            board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X" ||
+            board[0][1] == "X" && board[1][1] == "X" && board[2][1] == "X" ||
+            board[0][2] == "X" && board[1][2] == "X" && board[2][2] == "X" ||
+            board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X" ||
+            board[0][2] == "X" && board [1][1] == "X" && board[2][0] == "X"
+
+        ){
+            return true
+        }
+        else if(
+            board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O" ||
+            board[1][0] == "O" && board[1][1] == "O" && board[1][2] == "O" ||
+            board[2][0] == "O" && board[2][1] == "O" && board[2][2] == "O" ||
+            board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O" ||
+            board[0][1] == "O" && board[1][1] == "O" && board[2][1] == "O" ||
+            board[0][2] == "O" && board[1][2] == "O" && board[2][2] == "O" ||
+            board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O" ||
+            board[0][2] == "O" && board [1][1] == "O" && board[2][0] == "O"){
+                return true
+            }
+
+        else{
+            return false
+        }
+
+        
+    }
+
+    const isMarked = (currentMark) => {
+        return currentMark !== "0"
+    }
+
+    const checkTie = () => board.every(row => row.every(isMarked));
+
     
     const printBoard = () => {
-        // for(let i = 0; i < rows; i++){
-        //     for(let j = 0; j < cols; j++){
-        //         console.log("row: " + i + " " + "col: " + j + " " + board[i][j]);
-        //     }
-        // }
+        //prints a table visualization of the board
         console.table(board)
       };
 
-      return {getBoard, placeMark, printBoard}
+      return {getBoard, placeMark, printBoard, resetBoard, checkWin, checkTie}
 })()
 
 //PLAYER
@@ -80,6 +121,23 @@ const game = (function (){
         }
         else
         {
+            if(board.checkWin()){
+                board.printBoard()
+                console.log(`${getActivePlayer().name} won! Starting new game.`)
+                board.resetBoard()
+                activePlayer = players[0];
+                printNewRound()
+                return
+            }
+
+            if(board.checkTie() && !board.checkWin()){
+                board.printBoard()
+                console.log("It's a tie!")
+                board.resetBoard()
+                activePlayer = players[0];
+                printNewRound()
+                return
+            }
             console.log(`Placed ${getActivePlayer().playerMark} for ${getActivePlayer().name}.`)
 
             switchTurns();
@@ -88,6 +146,7 @@ const game = (function (){
         }
     }
     
+    board.resetBoard()
     printNewRound()
 
     return {playRound, getActivePlayer}
